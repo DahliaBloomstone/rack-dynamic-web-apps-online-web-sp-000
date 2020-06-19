00cc47cff048ef63d45dae7ecdd12ac99d9f2ab3
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 22:45:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,3 +11,5 @@ end
 
 #Then run it with rackup config.ru
 #port=60658 shows which port we'll have access the application on in the browser
+#host? localhost
+#Your server is running at 157.230.220.151:60658 is the full URL
