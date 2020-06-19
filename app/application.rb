e4f9a7b162587d823793ce34de070fd642e32362
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 22:46:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,34 @@
-class Application
+#class Application
 
-  def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
-  end
-
-end
-
+#  def call(env)
+#    resp = Rack::Response.new
+#    resp.write "Hello, World"
+#    resp.finish
+#  end
+#end
 
 #Then run it with rackup config.ru
 #port=60658 shows which port we'll have access the application on in the browser
 #host? localhost (IN IDE IT IS THE IP)
 #Your server is running at 157.230.220.151:60658 is the full URL
 #=>Hello, World on page
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
+    resp.finish
+  end
+
+end
